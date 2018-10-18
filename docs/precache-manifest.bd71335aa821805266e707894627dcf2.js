80760a9eceba4fd4275c735082d18ebce2d93891
estherpato <estherpato@gmail.com>
2018-10-18 22:05:45 +0200

    Deploy

---
+++ docs/precache-manifest.bd71335aa821805266e707894627dcf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "41481028ee9088cc3740",
+    "url": "./static/js/main.41481028.chunk.js"
+  },
+  {
+    "revision": "70c9baf42a486075c1e6",
+    "url": "./static/js/1.70c9baf4.chunk.js"
+  },
+  {
+    "revision": "41481028ee9088cc3740",
+    "url": "./static/css/main.f393c574.chunk.css"
+  },
+  {
+    "revision": "69d2e3c59052290407304b1d69805061",
+    "url": "./index.html"
+  }
+];
